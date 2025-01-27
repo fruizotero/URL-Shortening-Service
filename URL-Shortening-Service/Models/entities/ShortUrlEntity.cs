5e40952593f69d899559110f447df175d5c55c55
Frank ruiz <frank.git96@gmail.com>
2025-01-27 11:44:33 +0100

    Add initial migration and ShortUrlEntity model for URL shortening service

---
+++ URL-Shortening-Service/Models/entities/ShortUrlEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace URL_Shortening_Service.Models.entities
+{
+    public class ShortUrlEntity
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+       public string ShortCode { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+    }
+}
