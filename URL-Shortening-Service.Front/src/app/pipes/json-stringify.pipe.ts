b44299acf9c5b8923d6dc38118f19a074c9a5dfb
Frank ruiz <frank.git96@gmail.com>
2025-02-02 15:12:40 +0100

    Feat: Add JsonStringifyPipe for formatting JSON output

---
+++ URL-Shortening-Service.Front/src/app/pipes/json-stringify.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'jsonStringify'
+})
+export class JsonStringifyPipe implements PipeTransform {
+
+  transform(value: any): string {
+    return JSON.stringify(value, null, 2);
+  }
+
+}
