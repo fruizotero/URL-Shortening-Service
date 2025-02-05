7f82db02aa77704405b3cdb15bfaf821f536c42e
Frank ruiz <frank.git96@gmail.com>
2025-02-05 07:32:05 +0100

    Feat: Implement database connection handling with environment variables and create ApplicationContextFactory

---
+++ URL-Shortening-Service/Context/ApplicationContextFactory .cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting.Server;
+
+namespace URL_Shortening_Service.Context
+{
+    public class ApplicationContextFactory : IDesignTimeDbContextFactory<ApplicationContext>
+    {
+        public ApplicationContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false)
+            .AddEnvironmentVariables()
+            .Build();
+
+            var connectionString = ConnectionStringProvider.GetConnectionString(configuration);
+
+
+            Console.WriteLine(connectionString);
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+            optionsBuilder.UseSqlServer(connectionString, sqlOptions =>
+            {
+                sqlOptions.EnableRetryOnFailure(maxRetryCount: 5, maxRetryDelay: TimeSpan.FromSeconds(10), errorNumbersToAdd: null);
+            });
+
+            return new ApplicationContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ URL-Shortening-Service/Context/ConnectionStringProvider.cs
@@ -0,0 +1,16 @@
+﻿namespace URL_Shortening_Service.Context
+{
+    public class ConnectionStringProvider
+    {
+        public static string GetConnectionString(IConfiguration configuration)
+        {
+            string userId = Environment.GetEnvironmentVariable("DB_USER_ID") ?? "sa";
+            string password = Environment.GetEnvironmentVariable("DB_PASSWORD") ?? "sa.Bd.123";
+            string server = Environment.GetEnvironmentVariable("DB_SERVER") ?? "LOCALHOST,1533";
+
+            string baseConnection = configuration.GetConnectionString("DefaultConnection");
+
+            return $"Server={server};{baseConnection}User Id={userId};Password={password};";
+        }
+    }
+}
