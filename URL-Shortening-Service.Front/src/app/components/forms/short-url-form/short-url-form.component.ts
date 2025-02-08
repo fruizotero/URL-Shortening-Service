aab878844ef877e26304d89b3b508480948f5459
Frank ruiz <frank.git96@gmail.com>
2025-02-08 13:03:01 +0100

    Feat: Enhance ShortUrlFormComponent to support dynamic label updates and new URL input for PUT requests

--- URL-Shortening-Service.Front/src/app/components/forms/short-url-form/short-url-form.component.ts
+++ URL-Shortening-Service.Front/src/app/components/forms/short-url-form/short-url-form.component.ts
@@ -1,30 +1,57 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { InputTextComponent } from "../../inputs/input-text/input-text.component";
+import { InputTextComponent } from '../../inputs/input-text/input-text.component';
 import { FormShortUrl } from '../../../interfaces/form-short-url';
-
 
 @Component({
   selector: 'app-short-url-form',
   imports: [ReactiveFormsModule, InputTextComponent],
   templateUrl: './short-url-form.component.html',
-  styleUrl: './short-url-form.component.css'
+  styleUrl: './short-url-form.component.css',
 })
 export class ShortUrlFormComponent {
-
   @Output() formSubmitted = new EventEmitter<FormShortUrl>();
+  label: string = 'Short URL';
+  placeholder: string = 'Enter a URL';
+  isUpdate = signal(false);
 
   form = new FormGroup({
-    method: new FormControl("get"),
-    shortUrl: new FormControl(""),
+    method: new FormControl('get'),
+    shortUrl: new FormControl(''),
+    newUrl: new FormControl(''),
   });
 
   // vincular mi formulario con el componente
 
   onSubmit() {
     this.formSubmitted.emit(this.form.value as FormShortUrl);
-   }
+  }
 
+  onchangeMethod(event: Event) {
 
+    let element = event.target as HTMLSelectElement;
+    let value = element.value;
+    this.isUpdate.set(false)
 
+    switch (value) {
+      case 'get':
+        this.label = 'Short URL';
+        break;
+
+      case 'post':
+        this.label = 'Add URL';
+        break;
+      case 'put':
+        this.label = 'Update URL';
+        this.isUpdate.set(true);
+        break;
+      case 'delete':
+        this.label = 'Delete URL';
+        break;
+
+      default:
+        break;
+     }
+
+  }
 }
